6362d0867fdccb6166ea4d078972f65fc2eaf3a2
Boris Wilhelms <mail@wille-zone.de>
2022-11-15 22:52:57 +0100

    chore: Cleanup

--- src/ProductsService/Data/Entities/Product.cs
+++ src/ProductsService/Data/Entities/Product.cs
@@ -2,12 +2,11 @@
 {
     public class Product
     {
-
         public Product()
         {
             
         }
-        
+       
         public Product(Guid id, string name, string description, IEnumerable<string> categories, double price)
         {
             Id = id;
--- src/ProductsService/Models/ProductModels.cs
+++ src/ProductsService/Models/ProductModels.cs
@@ -2,19 +2,19 @@ namespace ProductsService.Models;
 
 public class ProductListModel
 {
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public double Price { get; set; }
-    public CurrencyValue LocalPrice { get; set; }
+    public Guid Id { get; init; }
+    public string Name { get; init; } = String.Empty;
+    public string Description { get; init; } = String.Empty;
+    public double Price { get; init; } = 0;
+    public CurrencyValue? LocalPrice { get; set; } 
     public string? Details { get; set; }
 }
 
 public class ProductDetailsModel
 {
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string[] Categories { get; set; }
-    public double Price { get; set; }
+    public Guid Id { get; init; }
+    public string Name { get; init; } = String.Empty;
+    public string Description { get; init; } = String.Empty;
+    public string[] Categories { get; init; } = Array.Empty<string>();
+    public double Price { get; init; } = 0;
 }
